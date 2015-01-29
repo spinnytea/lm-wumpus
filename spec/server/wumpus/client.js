bfb2ab4967554f31b64599b2e66a49fecd9b39a5
Garrett Tjarks <spinnytea@gmail.com>
2015-01-29 18:49:54 -0500

    testing notes

---
+++ spec/server/wumpus/client.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// TODO basics of testing wumpus
+// create a socket
+// exercise the various commands we can call
+// ensure we get the results we expect
+// basically, pretend to be a client
+
+// TODO in depth test
+// create a room
+// solve the room

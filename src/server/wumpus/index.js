f94b08693bfba18b1b6e58cba857e098ec618025
Garrett Tjarks <spinnytea@gmail.com>
2015-03-08 18:26:01 -0400

    refactor setup

--- src/server/wumpus/index.js
+++ src/server/wumpus/index.js
@@ -23,95 +23,102 @@ exports.setup = function(io) {
     socket.on('command', function(str) { socket.emit('action', str); socket.emit('message', 'echo'); });
     socket.on('context', function() { socket.emit('context', subgraph.stringify(context.subgraph, true)); });
 
-    socket.on('actuator', function(str) {
-      // find the list of actions for this str
-      if(!context.keys['action_'+str]) {
-        socket.emit('message', 'actuator:'+str+'> not found');
-        return;
-      }
-
-      // load the actions
-      var list = blueprint.list([context.idea('action_'+str), context.idea('wumpus_world')]).map(blueprint.load);
-      // build a state // TODO should this be in context (probably instead of subgraph)
-      var bs = new blueprint.State(context.subgraph, list);
-
-      // find the first action that works and do it
-      var success = list.some(function(a) {
-        var result = a.tryTransition(bs);
-        if(result.length > 0) {
-          a.runBlueprint(bs, result[0]);
-          if(result.length > 1)
-            console.log('more than one option?? ('+result.length+')');
-          return true;
-        }
-        return false;
-      });
-
-      if(success) {
-        socket.emit('message', 'actuator:'+str+'> potassium');
-      } else {
-        // if none of the actions work, report a message
-        socket.emit('message', 'actuator:'+str+'> could not apply');
-      }
-    });
-
-    socket.on('goal', function(str) {
-      // all plans are going to involve an agent
-      var goal = new subgraph.Subgraph();
-      var agentInstance = goal.addVertex(subgraph.matcher.filler);
-      goal.addEdge(
-        goal.addVertex(subgraph.matcher.id, context.idea('instance')),
-        links.list.thought_description,
-        agentInstance);
-      goal.addEdge(agentInstance, links.list.type_of,
-        goal.addVertex(subgraph.matcher.id, context.idea('agent')));
-      var roomDefinition = goal.addVertex(subgraph.matcher.id, context.idea('roomDefinition'));
-
-      if(str.indexOf('room') === 0) {
-        // the agent needs to be in the location we provide
-        var roomId = +str.substring(str.indexOf(' ')+1);
-        var loc = context.roomLoc[roomId];
-
-        // TODO specify new agent location based on room
-        var roomInstance = goal.addVertex(subgraph.matcher.discrete, { value: roomId, unit: context.idea('roomDefinition').id, loc: loc });
-        goal.addEdge(roomDefinition, links.list.thought_description, roomInstance);
-//        var agentLocation = goal.addVertex(subgraph.matcher.discrete, roomInstance, {transitionable:true,matchRef:true});
-        var agentLocation = goal.addVertex(subgraph.matcher.discrete, { value: roomId, unit: context.idea('roomDefinition').id, loc: loc }, {transitionable:true});
-        goal.addEdge(agentInstance, links.list.wumpus_sense_agent_loc, agentLocation);
-      } else {
-        socket.emit('message', 'goal:'+str+'> not a valid goal');
-        return;
-      }
-
-      var list = blueprint.list([context.idea('wumpus_world')]).map(blueprint.load);
-      var start = new blueprint.State(context.subgraph, list);
-      goal = new blueprint.State(goal, list);
-
-      if(start.matches(goal)) {
-        socket.emit('message', 'goal:'+str+'> here\'s the plan: do nothing');
-        return;
-      }
-
-      // TODO save serial plan
-      // - let me run it from the UI
-      // - make a delay
-      var sp = serialplan.create(start, goal);
-
-      if(sp) {
-        var result = sp.tryTransition(start);
-        if(result.length > 0) {
-          sp.runBlueprint(start, result[0]);
-          if(result.length > 1)
-            console.log('more than one result?? ('+result.length+')');
-          socket.emit('message', 'goal:'+str+'> oxygen potassium');
-        } else {
-          socket.emit('message', 'goal:'+str+'> could not apply the path ...?');
-        }
-      } else {
-        socket.emit('message', 'goal:'+str+'> could not find a path');
-      }
-    });
-
+    // take some action based on a message from the client
+    socket.on('actuator', exports.setup.actuator(socket));
+    socket.on('goal', exports.setup.goal(socket));
     socket.on('sense', context.sense);
   });
+};
+
+exports.setup.actuator = function(socket) {
+  return function(str) {
+    // find the list of actions for this str
+    if(!context.keys['action_'+str]) {
+      socket.emit('message', 'actuator:'+str+'> not found');
+      return;
+    }
+
+    // load the actions
+    var list = blueprint.list([context.idea('action_'+str), context.idea('wumpus_world')]).map(blueprint.load);
+    // build a state // TODO should this be in context (probably instead of subgraph)
+    var bs = new blueprint.State(context.subgraph, list);
+
+    // find the first action that works and do it
+    var success = list.some(function(a) {
+      var result = a.tryTransition(bs);
+      if(result.length > 0) {
+        a.runBlueprint(bs, result[0]);
+        if(result.length > 1)
+          console.log('more than one option?? ('+result.length+')');
+        return true;
+      }
+      return false;
+    });
+
+    if(success) {
+      socket.emit('message', 'actuator:'+str+'> potassium');
+    } else {
+      // if none of the actions work, report a message
+      socket.emit('message', 'actuator:'+str+'> could not apply');
+    }
+  };
+};
+
+exports.setup.goal = function(socket) {
+  return function(str) {
+    // all plans are going to involve an agent
+    var goal = new subgraph.Subgraph();
+    var agentInstance = goal.addVertex(subgraph.matcher.filler);
+    goal.addEdge(
+      goal.addVertex(subgraph.matcher.id, context.idea('instance')),
+      links.list.thought_description,
+      agentInstance);
+    goal.addEdge(agentInstance, links.list.type_of,
+      goal.addVertex(subgraph.matcher.id, context.idea('agent')));
+    var roomDefinition = goal.addVertex(subgraph.matcher.id, context.idea('roomDefinition'));
+
+    if(str.indexOf('room') === 0) {
+      // the agent needs to be in the location we provide
+      var roomId = +str.substring(str.indexOf(' ')+1);
+      var loc = context.roomLoc[roomId];
+
+      // TODO specify new agent location based on room
+      var roomInstance = goal.addVertex(subgraph.matcher.discrete, { value: roomId, unit: context.idea('roomDefinition').id, loc: loc });
+      goal.addEdge(roomDefinition, links.list.thought_description, roomInstance);
+//        var agentLocation = goal.addVertex(subgraph.matcher.discrete, roomInstance, {transitionable:true,matchRef:true});
+      var agentLocation = goal.addVertex(subgraph.matcher.discrete, { value: roomId, unit: context.idea('roomDefinition').id, loc: loc }, {transitionable:true});
+      goal.addEdge(agentInstance, links.list.wumpus_sense_agent_loc, agentLocation);
+    } else {
+      socket.emit('message', 'goal:'+str+'> not a valid goal');
+      return;
+    }
+
+    var list = blueprint.list([context.idea('wumpus_world')]).map(blueprint.load);
+    var start = new blueprint.State(context.subgraph, list);
+    goal = new blueprint.State(goal, list);
+
+    if(start.matches(goal)) {
+      socket.emit('message', 'goal:'+str+'> here\'s the plan: do nothing');
+      return;
+    }
+
+    // TODO save serial plan
+    // - let me run it from the UI
+    // - make a delay
+    var sp = serialplan.create(start, goal);
+
+    if(sp) {
+      var result = sp.tryTransition(start);
+      if(result.length > 0) {
+        sp.runBlueprint(start, result[0]);
+        if(result.length > 1)
+          console.log('more than one result?? ('+result.length+')');
+        socket.emit('message', 'goal:'+str+'> oxygen potassium');
+      } else {
+        socket.emit('message', 'goal:'+str+'> could not apply the path ...?');
+      }
+    } else {
+      socket.emit('message', 'goal:'+str+'> could not find a path');
+    }
+  };
 };

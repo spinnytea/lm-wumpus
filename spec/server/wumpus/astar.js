3a5212d310de479a77ee362b7eec4d92eea71633
Garrett Tjarks <spinnytea@gmail.com>
2015-03-24 20:50:42 -0400

    larger integration test

--- spec/server/wumpus/astar.js
+++ spec/server/wumpus/astar.js
@@ -6,6 +6,7 @@ var astar = require('lime/src/planning/algorithms/astar');
 var blueprint = require('lime/src/planning/primitives/blueprint');
 var links = require('lime/src/database/links');
 var Path = require('lime/src/planning/primitives/path');
+var serialplan = require('lime/src/planning/serialplan');
 var subgraph = require('lime/src/database/subgraph');
 
 var context = require('../../../src/server/wumpus/context');
@@ -54,6 +55,9 @@ function createStates(targetRoomId) {
 function actionNames(path) {
   return path.actions.map(function(a) { return a.action.substr(22); });
 }
+function stateRooms(path, vertexId) {
+  return path.states.map(function(s) { return s.state.vertices[vertexId].data.value; });
+}
 function summary(frontier) {
 
   // pull out all the elements in the frontier so we can have a good sorted order
@@ -94,6 +98,7 @@ describe('astar', function() {
   });
 
   it('small step-through', function() {
+    var agentLocation_vertexId = context.keys['agentLocation'];
     var states = createStates(68);
     var frontier = astar.units.frontier();
 
@@ -111,6 +116,7 @@ describe('astar', function() {
     astar.units.step(path, frontier);
 
     expect(actionNames(path)).to.deep.equal(['left']); // unlucky
+    expect(stateRooms(path, agentLocation_vertexId)).to.deep.equal([63, 63]);
     expect(frontier._elements.length).to.equal(5);
     expect(summary(frontier)).to.deep.equal([
       { comp: 8, cost: 2, dist: 3, p: ['right'] },
@@ -124,6 +130,7 @@ describe('astar', function() {
     astar.units.step(path, frontier);
 
     expect(actionNames(path)).to.deep.equal(['right']);
+    expect(stateRooms(path, agentLocation_vertexId)).to.deep.equal([63, 63]);
     expect(frontier._elements.length).to.equal(7);
     expect(summary(frontier)).to.deep.equal([
       { comp: 6, cost: 4, dist: 1, p: ['right', 'up'] },
@@ -139,6 +146,7 @@ describe('astar', function() {
     astar.units.step(path, frontier);
 
     expect(actionNames(path)).to.deep.equal(['right', 'up']);
+    expect(stateRooms(path, agentLocation_vertexId)).to.deep.equal([63, 63, 67]);
     expect(frontier._elements.length).to.equal(8);
     expect(summary(frontier)).to.deep.equal([
       { comp: 8, cost: 6, dist: 1, p: ['right', 'up', 'right'] },
@@ -155,6 +163,7 @@ describe('astar', function() {
     astar.units.step(path, frontier);
 
     expect(actionNames(path)).to.deep.equal(['right', 'up', 'left']); // unlucky
+    expect(stateRooms(path, agentLocation_vertexId)).to.deep.equal([63, 63, 67, 67]);
     expect(frontier._elements.length).to.equal(10);
     expect(summary(frontier)).to.deep.equal([
       { comp: 8, cost: 6, dist: 1, p: ['right', 'up', 'right'] },
@@ -173,6 +182,7 @@ describe('astar', function() {
     astar.units.step(path, frontier);
 
     expect(actionNames(path)).to.deep.equal(['right', 'up', 'right']);
+    expect(stateRooms(path, agentLocation_vertexId)).to.deep.equal([63, 63, 67, 67]);
     expect(frontier._elements.length).to.equal(12);
     expect(summary(frontier)).to.deep.equal([
       { comp: 8, cost: 8, dist: 0, p: ['right', 'up', 'right', 'up'] },
@@ -192,6 +202,148 @@ describe('astar', function() {
     path = frontier.deq();
 
     expect(actionNames(path)).to.deep.equal(['right', 'up', 'right', 'up']);
+    expect(stateRooms(path, agentLocation_vertexId)).to.deep.equal([63, 63, 67, 67, 68]);
     expect(path.last.matches(states.goal)).to.equal(true);
+  });
+
+  it('large step-through', function() {
+    var vertexId = context.keys['agentLocation'];
+    expect(context.idea('agentLocation').data().value).to.equal(63);
+    expect(context.subgraph.vertices[vertexId].data.value).to.equal(63);
+
+    var states = createStates(78);
+    var frontier = astar.units.frontier();
+
+    var path = new Path.Path([states.start], [], states.goal);
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal([]);
+    expect(stateRooms(path, vertexId)).to.deep.equal([63]);
+    expect(frontier._elements.length).to.equal(3);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['right']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([63, 63]);
+    expect(frontier._elements.length).to.equal(5);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([63, 63]);
+    expect(frontier._elements.length).to.equal(7);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'up']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([63, 63, 66]);
+    expect(frontier._elements.length).to.equal(9);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'up', 'up']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([63, 63, 66, 78]);
+    expect(frontier._elements.length).to.equal(10);
+
+    //
+    // okay, no that that's all done
+    // create a serial plan to get there and run it
+    //
+
+    expect(path.last.matches(states.goal)).to.equal(true);
+    expect(context.idea('agentLocation').data().value).to.equal(63);
+    expect(context.subgraph.vertices[vertexId].data.value).to.equal(63);
+
+    var sp = new serialplan.Action(path.actions);
+    var result = sp.tryTransition(states.start);
+    expect(result.length).to.equal(1);
+    sp.runBlueprint(states.start, result[0]);
+
+    expect(context.subgraph.vertices[vertexId].data.value).to.equal(78);
+    expect(context.idea('agentLocation').data().value).to.equal(78);
+    expect(context.idea('agentDirection').data().value).to.equal('north');
+
+    //
+    // and now do the same test going somewhere else
+    //
+
+    states = createStates(69);
+    frontier = astar.units.frontier();
+
+    path = new Path.Path([states.start], [], states.goal);
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal([]);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78]);
+    expect(frontier._elements.length).to.equal(2);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['right']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78]);
+    expect(frontier._elements.length).to.equal(3);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78]);
+    expect(frontier._elements.length).to.equal(4);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'right']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78]);
+    expect(frontier._elements.length).to.equal(5);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['right', 'left']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78]);
+    expect(frontier._elements.length).to.equal(6);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'left']); // finally, getting somewhere
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78]);
+    expect(frontier._elements.length).to.equal(8);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'left', 'up']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78, 66]);
+    expect(frontier._elements.length).to.equal(10);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'left', 'up', 'up']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78, 66, 63]);
+    expect(frontier._elements.length).to.equal(12);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'left', 'up', 'up', 'up']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78, 66, 63, 67]);
+    expect(frontier._elements.length).to.equal(13);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'left', 'up', 'up', 'up', 'right']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78, 66, 63, 67, 67]);
+    expect(frontier._elements.length).to.equal(15);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'left', 'up', 'up', 'up', 'left']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78, 66, 63, 67, 67]);
+    expect(frontier._elements.length).to.equal(17);
+
+    path = frontier.deq();
+    astar.units.step(path, frontier);
+    expect(actionNames(path)).to.deep.equal(['left', 'left', 'up', 'up', 'up', 'left', 'up']);
+    expect(stateRooms(path, vertexId)).to.deep.equal([78, 78, 78, 66, 63, 67, 67, 69]);
+    expect(frontier._elements.length).to.equal(18);
+
+    // the planning is at the goal
+    expect(path.last.matches(states.goal)).to.equal(true);
+    expect(context.idea('agentLocation').data().value).to.equal(78);
+    expect(context.subgraph.vertices[vertexId].data.value).to.equal(78);
   });
 }); // end astar

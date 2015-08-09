cfe5a953f883d205bb4e91f4c3b5e9a1fe821139
Garrett Tjarks <spinnytea@gmail.com>
2015-08-08 23:00:39 -0400

    task dump

--- src/client/js/todo/todoModule.js
+++ src/client/js/todo/todoModule.js
@@ -44,41 +44,6 @@ module.exports = angular.module('lime.client.todo', [
       ]
     },
     {
-      title: 'Integration Tests',
-      description: 'Really, this is more like an integration test. But we need something at this higher level. ' +
-      'The unit tests in lime test each thing independently. We need to test how they interact with each other, ' +
-      'how each of these intended use cases works. ' +
-      'Ultimately, I want to use these tests to debug and improve astar, ' +
-      'to figure ensure that all our many tools operate as desired, ' +
-      'to keep the whole project working.',
-      tasks: [
-        { text: 'use client to generate a room; save this as our example', status: 'done' },
-        { text: 'astar in-depth test: create/solve room', status: 'done' },
-        { text: 'server: actuator tests', status: 'done' },
-        { text: 'server: goal room', status: 'done' },
-        { text: 'agentLocation -> roomType (use in forward)', status: 'wontfix' },
-        { text: 'refactor tests', status: 'inprogress' },
-      ]
-    },
-    {
-      title: 'lm-wumpus general stuff',
-      description: 'These are just some nice-to-haves with lm-wumpus. ' +
-        'None of them are really important now that we have a working prototype, but it wouldn\'t hurt to have them.',
-      tasks: [
-        { text: 'add a delay for discrete so we can watch it play - where does this go?', status: 'done' },
-        { text: 'sense: alive, can only take actions when alive; allow up to enter pit; planning should prune this', status: 'done' },
-        { text: 'display the current plan on the UI', status: 'inprogress',
-          description: 'Needs to include fail/re-plan, needs to be more dynamic, allow plan to be canceled.' },
-        { text: 'make some sprites for the game', status: 'none' },
-        { text: 'fix the context direction', status: 'done' },
-        { text: 'allow "goto exit" and "goto gold" to work, even if there is "nothing to do"', status: 'done' },
-        { text: 'allow "play" to skip sections, then skip that part', status: 'done' },
-        { text: 'update subgraphModule to use the new subgraph.stringify format; remove the "wumpusModule.wumpusSocket.context" back-conversion', status: 'done' },
-        { text: 'address in code TODOs', status: 'none' },
-        { text: 'make wumpus/config a thing, so we can go there directly', status: 'none' },
-      ]
-    },
-    {
       title: 'Experience',
       description: 'You\'ve started thinking about it, start playing with it! Start with something simple like "turn left"',
       tasks: [
@@ -86,46 +51,6 @@ module.exports = angular.module('lime.client.todo', [
         { text: 'carve out a logic cycle', status: 'none', description: 'start with a separate entry point' },
         { text: 'recall experience', status: 'none' },
         { text: 'do science on experience', status: 'none', description: 'what mattered, what didn\'t' },
-      ]
-    },
-    {
-      title: 'Chance',
-      description: 'Run LM in chance:stochastic mode',
-      tasks: [
-        { text: 'check expected state against actual state; replan if mismatch', status: 'done' },
-      ]
-    },
-    {
-      title: 'Grain',
-      description: 'Run LM in grain:continuous mode',
-      tasks: [
-        { text: 'feature detection: identify rooms by loc', status: 'none' },
-      ]
-    },
-    {
-      title: 'Observable',
-      description: 'Run LM in observable:partially mode',
-      tasks: [
-        { text: 'explore until current goal is found', status: 'none' },
-        { text: 'how do we teach the agent to avoid pits?', status: 'none' },
-        { text: 'how do we teach the agent that the gold glitters', status: 'none' },
-        { text: 'navigate to nearest "unexplored room"', status: 'none',
-          description: 'this is how the maze game wanders the state space' },
-      ]
-    },
-    {
-      title: 'Timing',
-      description: 'Run LM in timing:dynamic mode',
-      tasks: [
-      ]
-    },
-    {
-      title: 'Complications',
-      description: 'Work in other types of complications that are environment simplifications',
-      tasks: [
-        { text: 'Sensor Noise: The values the sensors report aren\'t always perfect.', status: 'none' },
-        { text: 'Changing Environment: The environment doesn\'t always stay the same; rooms change/pits move/wumpus moves', status: 'none',
-          description: 'after you\'ve finished with a partially observable world' },
       ]
     },
   ];

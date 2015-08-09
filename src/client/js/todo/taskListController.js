78167eef93f05abcc7e899cd8a6e9dbe976fc813
Garrett Tjarks <spinnytea@gmail.com>
2015-08-09 18:02:22 -0400

    task list is no longer recursive

--- src/client/js/todo/taskListController.js
+++ src/client/js/todo/taskListController.js
@@ -5,15 +5,65 @@ module.exports.controller('lime.client.todo.taskList', [
   '$scope',
   '$http',
   function($scope, $http) {
-    if(!$scope.nestedId) {
-      $scope.nestedId = '';
-      $http.get('/rest/todo/statuses').success(function(data) { $scope.statuses = data.list.reduce(function(ret, obj) { ret[obj.id] = obj; return ret; }, {}); });
-      $http.get('/rest/todo/types').success(function(data) { $scope.types = data.list.reduce(function(ret, obj) { ret[obj.id] = obj; return ret; }, {}); });
+    $scope.tasks = [];
+    $http.get('/rest/todo/tasks?children=').success(function(data) { initViewData(data.list); $scope.tasks = data.list; });
+    $http.get('/rest/todo/statuses').success(function(data) { $scope.statuses = data.list.reduce(function(ret, obj) { ret[obj.id] = obj; return ret; }, {}); });
+    $http.get('/rest/todo/types').success(function(data) { $scope.types = data.list.reduce(function(ret, obj) { ret[obj.id] = obj; return ret; }, {}); });
+
+    $scope.viewData = {};
+    function initViewData(list, parent) {
+      var level = 0;
+      if(parent)
+        level = $scope.viewData[parent.id].level + 1;
+
+      list.forEach(function(task) {
+        $scope.viewData[task.id] = {
+          expanded: false,
+          level: level
+        };
+      });
     }
 
-    $scope.tasks = [];
-    $http.get('/rest/todo/tasks?children='+$scope.nestedId).success(function(data) { $scope.tasks = data.list; });
+    $scope.expand = function(task) {
+      if($scope.viewData[task.id].expanded) {
+        $scope.viewData[task.id].expanded = false;
 
-    $scope.expanded = {};
+        var remove = [];
+        collapse(task, remove);
+
+        $scope.tasks = $scope.tasks.filter(function(task) { return remove.indexOf(task.id) === -1; });
+      } else {
+        $scope.viewData[task.id].expanded = true;
+        $http.get('/rest/todo/tasks?children='+task.id).success(function(data) {
+          initViewData(data.list, task);
+          data.list.unshift($scope.tasks.indexOf(task)+1, 0);
+          $scope.tasks.splice.apply($scope.tasks, data.list);
+        });
+      }
+    };
+
+    function collapse(task, remove) {
+      Array.prototype.push.apply(remove, task.children);
+      task.children
+        .filter(function(id) { return $scope.viewData[id].expanded; })
+        .forEach(function(id) { collapse(findById(id), remove); });
+    }
+
+    //
+    // utils
+    //
+
+    $scope.range = function(num) {
+      return new Array(num);
+    };
+    function findById(id) {
+      var found;
+      $scope.tasks.some(function(task) {
+        if(task.id === id)
+          found = task;
+        return found;
+      });
+      return found;
+    }
   }
 ]);

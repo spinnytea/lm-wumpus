45bc24a3e0bf0efda9c328401fab162d32ec0895
Garrett Tjarks <spinnytea@gmail.com>
2015-08-13 17:15:20 -0400

    queries mock

--- src/client/js/todo/queries.js
+++ src/client/js/todo/queries.js
@@ -3,7 +3,22 @@
 module.exports = angular.module('lime.client.todo.queries', []);
 module.exports.controller('lime.client.todo.queriesController', [
   '$scope',
-  function($scope) {
-    void($scope);
+  '$http',
+  function($scope, $http) {
+    $scope.tasks = [];
+    $scope.formData = {};
+
+    $http.get('/rest/todo/statuses').success(function(data) { $scope.statuses = data.list; });
+
+    $scope.searching = false;
+    $scope.search = function() {
+      $scope.searching = true;
+      $http.get('/rest/todo/tasks?children=').success(function(data) {
+        $scope.searching = false;
+        $scope.tasks.splice(0);
+        Array.prototype.push.apply($scope.tasks, data.list);
+      });
+    };
+
   }
 ]);

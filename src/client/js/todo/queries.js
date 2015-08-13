112c8b4345d98f8394c5583b20b8497323163537
Garrett Tjarks <spinnytea@gmail.com>
2015-08-13 19:56:30 -0400

    task update; cache current query params

--- src/client/js/todo/queries.js
+++ src/client/js/todo/queries.js
@@ -1,12 +1,14 @@
 'use strict';
 
+var formData = {};
+
 module.exports = angular.module('lime.client.todo.queries', []);
 module.exports.controller('lime.client.todo.queriesController', [
   '$scope',
   '$http',
   function($scope, $http) {
     $scope.tasks = [];
-    $scope.formData = {};
+    $scope.formData = formData;
 
     $http.get('/rest/todo/types').success(function(data) { $scope.types = data.list.sort(function(a, b) { return b.order - a.order; }); });
     $http.get('/rest/todo/statuses').success(function(data) { $scope.statuses = data.list.sort(function(a, b) { return b.order - a.order; }); });
@@ -14,7 +16,7 @@ module.exports.controller('lime.client.todo.queriesController', [
     $scope.searching = false;
     $scope.search = function() {
       $scope.searching = true;
-      $http.get('/rest/todo/tasks', { params: $scope.formData }).success(function(data) {
+      $http.get('/rest/todo/tasks', { params: formData }).success(function(data) {
         $scope.searching = false;
         $scope.tasks.splice(0);
         Array.prototype.push.apply($scope.tasks, data.list.sort(function(a, b) {
@@ -27,5 +29,9 @@ module.exports.controller('lime.client.todo.queriesController', [
       });
     };
 
+    // if there is at least one field with specificity, then perform an initial search
+    if(Object.keys(formData).some(function(key) { return formData[key]; })) {
+      $scope.search();
+    }
   }
 ]);

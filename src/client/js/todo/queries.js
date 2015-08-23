696737ce7bb4ba6d26218f48dd17b3eca4ab2213
Garrett Tjarks <spinnytea@gmail.com>
2015-08-23 13:16:44 -0400

    recording ideas for experience

--- src/client/js/todo/queries.js
+++ src/client/js/todo/queries.js
@@ -14,9 +14,11 @@ module.exports.controller('lime.client.todo.queriesController', [
   function($scope, $http, statusService, typeService, priorityService) {
     $scope.tasks = [];
     $scope.formData = formData;
-    statusService.ready.then(function() { $scope.statuses = statusService.list; });
+    var statuses;
+    var priorities;
+    statusService.ready.then(function() { $scope.statuses = statusService.list; statuses = statusService.map; });
     typeService.ready.then(function() { $scope.types = typeService.list; });
-    priorityService.ready.then(function() { $scope.priorities = priorityService.list; });
+    priorityService.ready.then(function() { $scope.priorities = priorityService.list; priorities = priorityService.map; });
 
     $scope.searching = false;
     $scope.search = function() {
@@ -25,6 +27,14 @@ module.exports.controller('lime.client.todo.queriesController', [
         $scope.searching = false;
         $scope.tasks.splice(0);
         Array.prototype.push.apply($scope.tasks, data.list.sort(function(a, b) {
+          // sort by status category
+          if(statuses[b.status].categoryOrder !== statuses[a.status].categoryOrder)
+            return statuses[b.status].categoryOrder - statuses[a.status].categoryOrder;
+
+          // sort on priority
+          if(b.priority !== a.priority)
+            return priorities[b.priority].order - priorities[a.priority].order;
+
           // lexicographical sort on ID; highest first
           if(b.id.length !== a.id.length)
             return b.id.length - a.id.length;
